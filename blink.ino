5e87a6df4240a58b52e17298361a37d74a37b21f
maneeha426-collab <maneeha426@gmail.com>
2026-05-03 12:35:39 +0500

    Create blink.ino

---
+++ blink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED ON");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED OFF");
+  delay(1000);
+}
