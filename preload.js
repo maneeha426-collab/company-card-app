16f407cd207d5155add06f86389799a9ab37de7c
maneeha426-collab <maneeha426@gmail.com>
2026-05-03 12:45:51 +0500

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,9 @@
+// preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+    on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args))
+  }
+});
